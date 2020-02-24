e910523ea512e32afbdbcd6e814ee3c4360039aa
kikko2796 <kikko2796@gmail.com>
2020-02-24 02:53:27 +0000

    modify html for practice

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,15 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
   def new
+    @user=User.new
   end
   def create
     @user = User.new(name: params[:name], email:  params[:email])
-    @user.save
+    if @user.save
+      flash[:notice]="ユーザー登録が完了しました"
     redirect_to("/users/#{@user.id}")
+    else
+    render("users/new")
+    end
   end
 end
