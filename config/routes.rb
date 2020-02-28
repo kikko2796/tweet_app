f38980344ac26a7c30a2df04184051cfbc3c59b6
kikko2796 <kikko2796@gmail.com>
2020-02-28 12:13:51 +0000

    modify html for practice

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+   get "login" => "users#login_form"
   post "login" => "users#login"
-  get "login" => "users#login_form"
+ post "logout" => "users#logout"
+ 
 post "users/:id/update" => "users#update"
+get "users/:id/edit" => "users#edit"
   post 'users/create' => "users#create"
   get '/signup' => "users#new"
 get 'users/index' => "users#index"
-get "users/:id/edit" => "users#edit"
+
 get "users/:id" => "users#show"
 
   get 'posts/index' =>"posts#index"
