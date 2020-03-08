250acf7f95cf4a811063d1b9beb342c4951bb849
kikko2796 <kikko2796@gmail.com>
2020-03-08 13:12:09 +0000

    modify html for practice

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,30 @@
 class PostsController < ApplicationController
   before_action :authenticate_user
+  before_action :ensure_correct_user,{only:[:edit, :update, :destroy]}
   
   def index
-    @posts=Post.all.order(created_at: :desc)
+    @posts = Post.all.order(created_at: :desc)
   end
+  
   def show
-    @post=Post.find_by(id:params[:id])
+    @post = Post.find_by(id: params[:id])
+    # 変数@userを定義してください
+    @user = @post.user
     
   end
+  
   def new 
-    @post=Post.new
+    @post = Post.new
   end
+  
   def create
-    @post=Post.new(content: params[:content])
+    @post = Post.new(
+      content: params[:content],
+      user_id: @current_user.id
+    )
+  
   if @post.save
-     flash[:notice]="投稿を作成しました"
+     flash[:notice] = "投稿を作成しました"
     redirect_to("/posts/index")
   else
     render("/posts/new")
@@ -40,7 +50,12 @@ class PostsController < ApplicationController
    flash[:notice]="投稿を削除しました"
     redirect_to("/posts/index")
   end
+  
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    if @post.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
+  end
 end
-    
- 
-
